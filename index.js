1b3c992dcdf1501718a191047ac7728e78014b18
sason26mucha <74131853+sason26mucha@users.noreply.github.com>
2020-12-30 08:28:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// const hikeExp = document.querySelector('.hike-exp');
+
+
+
+// document.addEventListener('scroll,',scrollReveal);
+
+
+
+// function scrollReveal(){
+//     const hikePos =hikeExp.getBoundingClientRect();
+//     console.log(hikePos);
+ 
+// }
+// scrollReveal();
